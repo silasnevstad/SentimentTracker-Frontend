c92c8bf5111f0cd89f950ca6f489c977e20f1e8a
Silas Nevstad <nevstads@gmail.com>
2023-04-16 01:06:43 -0400

    new API setup

--- src/components/api/useSentiment.js
+++ src/components/api/useSentiment.js
@@ -13,12 +13,20 @@ const useSentiment = (keyword, setIsLoading, setError, filter) => {
       console.log('model:', model)
 
       // make url, if keyword has hashtags, replace with %23
-      let url = `http://senttrac-backend-dev.us-west-2.elasticbeanstalk.com/sentiment?keyword=${keyword}&model=${model}`//`http://0.0.0.0:8000/sentiment?keyword=${keyword}&model=${model}` // `http://senitment--backend-env.eba-tpnwk3fv.us-west-2.elasticbeanstalk.com/sentiment?keyword=${keyword}&model=${model}`; // `http://127.0.0.1:5000/sentiment?keyword=${keyword}&model=${model}`
+      let url = `http://senttrac-backend-dev.us-west-2.elasticbeanstalk.com/sentiment`//`http://0.0.0.0:8000/sentiment?keyword=${keyword}&model=${model}` // `http://senitment--backend-env.eba-tpnwk3fv.us-west-2.elasticbeanstalk.com/sentiment?keyword=${keyword}&model=${model}`; // `http://127.0.0.1:5000/sentiment?keyword=${keyword}&model=${model}`
       url = url.replace(/#/g, '%23');
 
       // Fetch sentiment summary from Flask API...
       axios.get(url, {
         method: 'GET',
+        params: {
+          keyword: keyword,
+          model: model,
+        },
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+        },
         async: true, 
         timeout: 0,
       })
