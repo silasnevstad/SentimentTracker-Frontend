2437e62153e1a6801996ea90c6941597dab7e5a3
Silas Nevstad <nevstads@gmail.com>
2023-04-16 23:38:23 -0400

    Heroku Api setup

--- src/components/api/useSentiment.js
+++ src/components/api/useSentiment.js
@@ -11,7 +11,7 @@ const useSentiment = (keyword, setIsLoading, setError, filter) => {
       let model = filter === 'precision' ? 'gpt-4' : 'gpt-3.5-turbo';
 
       // make url, if keyword has hashtags, replace with %23
-      let url = `https://sent-backend-dev.us-west-2.elasticbeanstalk.com/sentiment` //`https://senttrac-backend-dev.us-west-2.elasticbeanstalk.com/sentiment`//`http://0.0.0.0:8000/sentiment?keyword=${keyword}&model=${model}` // `http://senitment--backend-env.eba-tpnwk3fv.us-west-2.elasticbeanstalk.com/sentiment?keyword=${keyword}&model=${model}`; // `http://127.0.0.1:5000/sentiment?keyword=${keyword}&model=${model}`
+      let url = `https://boiling-lake-10566.herokuapp.com/sentiment` //`https://senttrac-backend-dev.us-west-2.elasticbeanstalk.com/sentiment`//`http://0.0.0.0:8000/sentiment?keyword=${keyword}&model=${model}` // `http://senitment--backend-env.eba-tpnwk3fv.us-west-2.elasticbeanstalk.com/sentiment?keyword=${keyword}&model=${model}`; // `http://127.0.0.1:5000/sentiment?keyword=${keyword}&model=${model}`
       url = url.replace(/#/g, '%23');
 
       // Fetch sentiment summary from Flask API...
@@ -22,11 +22,11 @@ const useSentiment = (keyword, setIsLoading, setError, filter) => {
           model: model,
         },
         headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*',
+          // 'Content-Type': 'application/json',
+          // 'Access-Control-Allow-Origin': '*',
         },
         async: true, 
-        timeout: 0,
+        timeout: 10000000000,
       })
         .then((response) => {
           console.log(response)
