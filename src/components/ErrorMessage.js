403d2d22306faf294da5e441c6be7fd78919eeb2
Silas Nevstad <nevstads@gmail.com>
2023-04-17 22:03:02 -0400

    New error message

--- src/components/ErrorMessage.js
+++ src/components/ErrorMessage.js
@@ -6,7 +6,7 @@ function ErrorMessage() {
         <div className="intro-message-container">
             <div className="intro-message">
                 <text className="intro-message-title">
-                    Error retrieving data. Please try again.
+                    No sources found for this query. Please try again.
                 </text>
                 <text className="intro-message-text">
                 {/* Our AI-powered tool analyzes recent tweets, offering a visual representation of emotions and their intensity. With this information, gain insight into the prevailing emotions associated with your query, presented in an easy-to-understand format */}
